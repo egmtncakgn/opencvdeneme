37f0d6d60a599d6dda90783b5ee0866f79e64e3d
egmtncakgn <89608701+egmtncakgn@users.noreply.github.com>
2024-05-25 21:12:49 +0300

    Ders 1 Eklendı

---
+++ ders1.cpp
@@ -0,0 +1,74 @@
+// 25.05.24 Murtaza reisin derslerinden ilerliyorum
+
+#include <opencv2/imgcodecs/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+// Main metodunun kodlarýmýzý görmesini saðlýyoruz
+static void VideoOkuma();
+static void ResimOkuma();
+static void WebCamOkuma();
+
+
+void main() {
+	VideoOkuma();
+}
+
+// Resim Okuma kodlarý
+static void ResimOkuma() {
+	string yol = "Resources/test.png";
+
+	// Resmi matrix e dönüþtürüyoruz
+	Mat res = imread(yol);
+
+	// Ekranda göster 
+	imshow("Test Ekraný", res);
+	waitKey(0);
+
+}
+
+// Videodan okuma kodlarý
+static void VideoOkuma() {
+	string yol = "Resources/test_video.mp4";
+	VideoCapture cap(yol);
+	Mat res;
+	while (true)
+	{
+		// imread yerine capureden gelen res matrixinin içine
+		// yazýlýyor
+		cap.read(res);
+
+		// Ekranda göster 
+		imshow("Test Ekraný", res);
+		
+		// Burada waitKey deðeri önemli videonun fpsini belirliyor
+		// Eðer 0 girilirse kullanýcýdan input bekliyor
+		waitKey(25);
+	}
+}
+
+// WebCamden okuma kodlarý
+static void WebCamOkuma() {
+	// Cap() içine cihazýn idsi girilerek farklý aygýtlardan görüntü
+	// alýnabilir
+	VideoCapture cap(0);
+	Mat res;
+
+	while (true)
+	{
+		// imread yerine capureden gelen res matrixinin içine
+		// yazýlýyor
+		cap.read(res);
+
+		// Ekranda göster 
+		imshow("Test Ekraný", res);
+
+		// Burada waitKey deðeri önemli videonun fpsini belirliyor
+		// Eðer 0 girilirse kullanýcýdan input bekliyor
+		waitKey(1);
+	}
+}
--- opencvdeneme.cpp
+++ opencvdeneme.cpp
@@ -1,16 +1,28 @@
-﻿// 24.05.24 Staj için openCv ve c++ denemeleri
+﻿// 24.05.24 Staj için openCv ve c++ denemeleri için proje oluşturuldu
+// Egmen Tunç Akgün
 
-#include <opencv2/opencv.hpp>
+// 25.05.24 Build hatası çözüldü
+// "Hata LNK1104 'opencv_world450d.lib' dosyası açılamıyor" 
+// Hatası aldım sebebi openCV nin 4.5 versiyonu için olan tutoriali takip etmem
+// benim kullandığım sürüm 4.9 'opencv_world450d.lib' yerine 'opencv_world490d.lib'
+// yazdım düzeldi 
 
-int main()
-{
 
-    cv::Mat image = cv::imread("path_to_image.jpg");
-    if (image.empty()) {
-        std::cout << "Could not open or find the image!" << std::endl;
-        return -1;
-    }
-    cv::imshow("Display window", image);
-    cv::waitKey(0); // Wait for a keystroke in the window
-    return 0;
-}
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+// Böyle kullanırsak :: yazmamıza gerek yok :)
+using namespace cv;
+using namespace std;
+
+
+/*void main() {
+
+    string path = "Resources/test.png";
+    Mat img = imread(path);
+    imshow("Image", img);
+    waitKey(0);
+
+}*/
